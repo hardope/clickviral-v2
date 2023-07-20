4b4074b4b1154327d73cae23e53e1185e9355e72
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-07-20 18:48:05 +0100

    Initial Commit

---
+++ users/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Create your views here.
+@api_view(['GET'])
+def home(request):
+     return Response({'message':'Hello World'})
