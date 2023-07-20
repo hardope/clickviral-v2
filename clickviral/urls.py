3cc484d856176273a7dba561090b0c732805797d
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-07-20 19:50:19 +0100

    Swagger Documentation

--- clickviral/urls.py
+++ clickviral/urls.py
@@ -16,7 +16,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view as swagger_get_schema_view
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title = "ClickViral API",
+        default_version = 'v2',
+        description="API for ClickViral",
+    ),
+    public=True,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/',include('users.urls')),
+
+     path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
