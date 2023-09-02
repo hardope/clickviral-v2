c8c02f45f6fd4a5838076917d0bc27eaeec62c8b
hardope <adeyeriopeoluwa05@gmail.com>
2023-09-02 09:54:44 +0100

    secret and settings

--- clickviral/settings.py
+++ clickviral/settings.py
@@ -11,21 +11,25 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import json
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+with open(os.path.join(BASE_DIR, 'secret_key.json')) as f:
+    SECRET = json.loads(f.read())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qd@x8pnlfhy_m93o%nxh32ewr_i2g+3e@b(1n8w9xzp#*)20fe'
+SECRET_KEY = SECRET['secret_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = SECRET['debug']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = SECRET['allowed_hosts']
 
 
 # Application definition
@@ -75,8 +79,12 @@ WSGI_APPLICATION = 'clickviral.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': SECRET['database_engine'],
+        'NAME': SECRET['database_name'],
+        'USER': SECRET['database_user'],
+        'PASSWORD': SECRET['database_password'],
+        'HOST': SECRET['database_host'],
+        'PORT': SECRET['database_port'],
     }
 }
 
@@ -116,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
