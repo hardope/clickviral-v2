97e54e7dc3230a6e9f431b465b3e505e4c85ba68
hardope <adeyeriopeoluwa05@gmail.com>
2023-09-03 09:57:56 +0100

    Initial Done

--- clickviral/settings.py
+++ clickviral/settings.py
@@ -17,7 +17,7 @@ import json
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-with open(os.path.join(BASE_DIR, 'secret_key.json')) as f:
+with open(os.path.join(BASE_DIR, 'secret.json')) as f:
     SECRET = json.loads(f.read())
 
 # Quick-start development settings - unsuitable for production
