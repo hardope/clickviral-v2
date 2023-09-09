e0b025cb830586e916e3ffb561bc06fd1736fab3
hardope <adeyeriopeoluwa05@gmail.com>
2023-09-09 12:27:23 +0100

    Models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from uuid import uuid4
 
-# Create your models here.
+class Profile(User):
+
+    bio = models.TextField(max_length=200, blank=True)
+    location = models.TextField(max_length=50, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+
+        verbose_name = 'Profile'
+        verbose_name_plural = 'Profiles'
+        ordering = ['username']
