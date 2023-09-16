4073ef864a8546905af01ff325a0b6983b6fc7b9
hardope <adeyeriopeoluwa05@gmail.com>
2023-09-16 12:28:05 +0100

    Models

--- account/models.py
+++ account/models.py
@@ -22,3 +22,29 @@ class Account(models.Model):
     user = models.OneToOneField(Profile, on_delete=models.CASCADE)
     activation_key = models.UUIDField(default=uuid4, editable=False)
 
+class Preferences(models.Model):
+
+    theme_options = (
+        ('light', 'Light'),
+        ('dark', 'Dark'),
+    )
+
+    birthday_display_preference = (
+        ('y', 'Yes'),
+        ('n', 'No'),
+    )
+
+    birth_date_display_preference = (
+        ('y', 'Yes'),
+        ('n', 'No'),
+    )
+
+    user = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    theme = models.CharField(max_length=5, choices=theme_options, default='light')
+    display_age = models.CharField(max_length=1, choices=birthday_display_preference, default='y')
+    display_birth_date = models.CharField(max_length=1, choices=birth_date_display_preference, default='y')
+    chat_notifications_mail = models.BooleanField(default=True)
+    comment_notifications_mail = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.user.username
