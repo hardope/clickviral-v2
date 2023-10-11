79bcfcdfd295dc0ffe222b14d9739e3736b45b7e
hardope <adeyeriopeoluwa05@gmail.com>
2023-10-11 22:10:41 +0100

    urls & serializers

--- account/serializers.py
+++ account/serializers.py
@@ -1,6 +1,7 @@
 from .models import Profile, Account, Preferences
 from rest_framework import serializers
 from django.contrib.auth.password_validation import validate_password
+from comms import Email
 
 class ProfileSerializer(serializers.ModelSerializer):
 
@@ -17,24 +18,38 @@ class ProfileSerializer(serializers.ModelSerializer):
             'location',
             'birth_date',
             'is_active',
-            'joined_at',
+            'date_joined',
         ]
+
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
     def create(self, validated_data):
         user = Profile.objects.create(**validated_data)
         user.is_active = False
         user.is_staff = False
         user.is_superuser = False
+
+        account = Account.objects.create(user=user)
+        preferences = Preferences.objects.create(user=user)
+
+        domain = self.context['request'].META['HTTP_HOST']
+
+        mail = Email(to=user.email, domain=domain)
+        mail.send_sign_up(account.activation_key, user.id)
         user.save()
+
         return user
     
     def validate(self, attrs):
-        
+
+        if not (attrs.get('email')):
+            raise serializers.ValidationError({'email': 'provide a valid email address'})
         if Profile.objects.filter(email=attrs['email']).exists():
             raise serializers.ValidationError({'email': 'Email is already in use.'})
         if Profile.objects.filter(username=attrs['username']).exists():
             raise serializers.ValidationError({'username': 'Username is already in use.'})
-        validate_password(attrs['password'])
         
         if self.context['request'].method == 'PUT':
             if attrs.get('email', False):
--- account/urls.py
+++ account/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('profile/<int:pk>/', views.OneProfile.as_view()),
+    path('account/create', views.CreateProfile.as_view()),
+]
