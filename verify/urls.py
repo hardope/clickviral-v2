7c00f751a075ed970411172f2e9ffe1d809a1036
hardope <adeyeriopeoluwa05@gmail.com>
2023-10-24 17:22:23 +0100

    verify password and email

--- verify/urls.py
+++ verify/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('account/<int:id>/<str:code>/', views.verify_account)
+    path('account/<int:id>/<str:code>/', views.verify_account),
+    path('email/<str:code>/', views.verify_email),
+    path('password/<str:code>/', views.verify_password),
 ]
--- verify/views.py
+++ verify/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from account.models import Profile, Account
+from utils import decode
+
 # Create your views here.
 
 def verify_account(request, id, code):
@@ -15,3 +17,31 @@ def verify_account(request, id, code):
         return HttpResponse("<h1>Account verified!. Proceed to login</h1>")
     else:
         return HttpResponse("<h1>Invalid verification code!</h1>")
+    
+
+def verify_email(request, code):
+    try:
+        data = decode(code)
+        profile = Profile.objects.get(id=data['user_id'])
+    except Exception as e:
+        return HttpResponse(f"<h1>Invalid verification code! {e}</h1>")
+    except:
+        return HttpResponse("<h1>Invalid verification code!</h1>")
+    if Profile.objects.filter(email=data['email']).exists():
+        return HttpResponse("<h1>Email already in use!</h1>")
+    profile.email = data['email']
+    profile.save()
+    return HttpResponse("<h1>Email verified!</h1>")
+
+
+def verify_password(request, code):
+    try:
+        data = decode(code)
+        profile = Profile.objects.get(id=data['user_id'])
+    except Exception as e:
+        return HttpResponse(f"<h1>Invalid verification code! {e}</h1>")
+    except:
+        return HttpResponse("<h1>Invalid verification code!</h1>")
+    profile.set_password(data['password'])
+    profile.save()
+    return HttpResponse("<h1>Password reset!</h1>")
