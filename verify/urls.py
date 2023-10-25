7e59b58a764c6e58647a6c70e794bbda45a917d7
hardope <adeyeriopeoluwa05@gmail.com>
2023-10-25 15:46:44 +0100

    Updated verify for user new token verifications

--- verify/urls.py
+++ verify/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('account/<int:id>/<str:code>/', views.verify_account),
-    path('email/<str:code>/', views.verify_email),
-    path('password/<str:code>/', views.verify_password),
+    path('account/<str:token>/', views.verify_account),
+    path('email/<str:token>/', views.verify_email),
+    path('password/<str:token>/', views.verify_password),
 ]
--- verify/views.py
+++ verify/views.py
@@ -1,32 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from account.models import Profile, Account
+from account.models import Profile
 from utils import decode
 
 # Create your views here.
 
-def verify_account(request, id, code):
-    try:
-        profile = Profile.objects.get(id=id)
-    except:
-        return HttpResponse("<h1>Invalid verification code!</h1>")
-    account = Account.objects.get(user=profile)
-    if str(account.activation_key) == code:
-        profile.is_active = True
-        profile.save()
-        return HttpResponse("<h1>Account verified!. Proceed to login</h1>")
-    else:
-        return HttpResponse("<h1>Invalid verification code!</h1>")
-    
+def verify_account(request, token):
 
-def verify_email(request, code):
     try:
-        data = decode(code)
-        profile = Profile.objects.get(id=data['user_id'])
-    except Exception as e:
-        return HttpResponse(f"<h1>Invalid verification code! {e}</h1>")
+        data = decode(token)
+        profile = Profile.objects.get(id=data['user_id'], email=data['email'])
     except:
-        return HttpResponse("<h1>Invalid verification code!</h1>")
+        return HttpResponse("<h1>Invalid verification Token!</h1>")
+    
+    profile.is_active = True
+    profile.save()
+    return HttpResponse("<h1>Account verified!</h1>")
+
+def verify_email(request, token):
+    try:
+        data = decode(token)
+        profile = Profile.objects.get(id=data['user_id'])
+    except:
+        return HttpResponse("<h1>Invalid verification Token!</h1>")
     if Profile.objects.filter(email=data['email']).exists():
         return HttpResponse("<h1>Email already in use!</h1>")
     profile.email = data['email']
@@ -34,14 +30,12 @@ def verify_email(request, code):
     return HttpResponse("<h1>Email verified!</h1>")
 
 
-def verify_password(request, code):
+def verify_password(request, token):
     try:
-        data = decode(code)
+        data = decode(token)
         profile = Profile.objects.get(id=data['user_id'])
-    except Exception as e:
-        return HttpResponse(f"<h1>Invalid verification code! {e}</h1>")
     except:
-        return HttpResponse("<h1>Invalid verification code!</h1>")
+        return HttpResponse("<h1>Invalid verification Token!</h1>")
     profile.set_password(data['password'])
     profile.save()
     return HttpResponse("<h1>Password reset!</h1>")
