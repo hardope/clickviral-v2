0b35812a6d395c0aab1e7985156fe11f5ab725b1
hardope <adeyeriopeoluwa05@gmail.com>
2024-02-14 01:15:37 +0100

    Reset

--- account/views/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-
-files = os.listdir(os.path.dirname(__file__))
-__all__ = [file[:-3] for file in files if file.endswith('.py') and file != '__init__.py']
-
-for file in __all__:
-    exec(f'from .{file} import *')
--- verify/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('account/<str:token>/', views.verify_account),
-    path('email/<str:token>/', views.verify_email),
-    path('password/<str:token>/', views.verify_password),
-]
