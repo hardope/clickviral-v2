0923951c9d4d92e84d3a614cec29cac4da5f91cc
hardope <adeyeriopeoluwa05@gmail.com>
2024-04-15 00:29:02 +0000

    Update Logger to show response status

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,8 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 
 const logger = () => {
-    return (req: Request, _res: Response, next: NextFunction) => {
-        console.log(`[${new Date().toISOString()}] -- ${req.method} -- ${req.path}`);
+    return (req: Request, res: Response, next: NextFunction) => {
+        res.on("finish", () => {
+            console.log(`[${new Date().toISOString()}] -- ${req.method} -- ${req.path} -- ${res.statusCode}`);
+        });
         next();
     };
 }
