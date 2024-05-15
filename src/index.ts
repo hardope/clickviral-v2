89e059d7e67b548e4f138b10ef4092dd6ea9030d
hardope <adeyeriopeoluwa05@gmail.com>
2024-05-15 17:30:32 +0100

    CORS

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,27 @@ import bodyParser from 'body-parser';
 import logger from './middleware/logger';
 import { PORT } from './utils/environment';
 import fileUpload from 'express-fileupload';
+import cors from 'cors';
+
+const allowedHosts = [
+    'http://localhost:8000',
+    // Add more as needed
+];
+
+const corsOptions = {
+    origin: (origin: any, callback: any) => {
+        if (allowedHosts.includes(origin)) {
+            return callback(null, true);
+        }
+        callback(new Error('Not allowed by CORS'));
+    }
+};
 
 const app = express();
 connectDB();
 
 app.use(bodyParser.json());
+app.use(cors(corsOptions));
 app.use(logger());
 app.use(fileUpload());
 
