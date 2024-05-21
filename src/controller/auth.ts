f7ce091c746ee6f7415f3b8204832610269bb1cf
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2024-05-21 14:54:01 +0000

    verify Opt for password reset

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -110,6 +110,42 @@ const forgotPassword = () => {
     }
 }
 
+const verifyResetOtp = () => {
+    return async (req: Request, res: Response) => {
+        try {
+            const email = req.body.email;
+            const otp = req.body.otp;
+            const user = await User.findOne({ email: email });
+
+            if (!user) {
+                res.status(404).send({
+                    "message": "User not found",
+                    "status": "not_found"
+                });
+            } else {
+                const otpRecord = await Otp.findOne({user_id: user._id, otp: otp, purpose: "password_reset"});
+
+                if (!otpRecord) {
+                    res.status(401).send({
+                        "message": "Invalid OTP",
+                        "status": "unauthorized"
+                    });
+                } else {
+                    res.status(200).send({
+                        "message": "OTP verified successfully",
+                        "status": "success"
+                    });
+                }
+            }
+        } catch (error) {
+            res.status(500).send({
+                "message": "An error occurred while verifying OTP",
+                "status": "error"
+            });
+        }
+    }
+}
+
 const resetPassword = () => {
     return async (req: Request, res: Response) => {
         try {
@@ -345,4 +381,13 @@ const twoFactorLogin = () => {
     }
 }
 
-export { login, forgotPassword, resetPassword, startResetEmail, changeEmail, updateSecurity, getSecurity, twoFactorLogin };
+export {
+    login,
+    forgotPassword,resetPassword,
+    startResetEmail,
+    changeEmail,
+    updateSecurity,
+    getSecurity,
+    twoFactorLogin,
+    verifyResetOtp,
+};
