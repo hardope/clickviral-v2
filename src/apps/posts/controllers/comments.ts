f690722082f51a0ab51e20fbb6483d0cd96ea689
hardope <adeyeriopeoluwa05@gmail.com>
2025-01-02 23:34:38 +0100

    hotfix

--- src/apps/posts/controllers/comments.ts
+++ src/apps/posts/controllers/comments.ts
@@ -4,11 +4,12 @@ import { Posts } from '../models';
 const getComments = () => {
     return async (req: Request, res: Response) => {
         const comments = await Posts.find({ type: 'comment', parent_post_id: req.params.id }).populate('user');
-
+        const post = await Posts.findById(req.params.id).populate('user');
         const formattedComments = await Promise.all(await comments.map(async comment => await comment.toJSON(req.user)));
 
         res.status(200).send({
-            data: formattedComments,
+            post: await post?.toJSON(req.user),
+            comments: formattedComments,
             message: 'Comments retrieved successfully',
             status: 'success'
         });
